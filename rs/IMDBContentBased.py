f3e5cb68868bf8adce192236e0527f82497fc2fa
Matheus Santos <tetofsa15@gmail.com>
2019-06-03 12:12:10 -0300

    Criação do modulo para coleta de avaliações

--- rs/IMDBContentBased.py
+++ rs/IMDBContentBased.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 # Libraries for text preprocessing
 import re
 import nltk
@@ -17,7 +18,10 @@ class IMDBContentBased:
     def __init__(self):
         self.df_imdb = pd.read_csv('./datasets/IMDB-Movie-Data.csv')
         self.X = pd.DataFrame()
+        self.y = []
+        self.X_train = pd.DataFrame()
     # Most frequently occuring words
+
     def get_top_n_words(corpus, n=None):
         vec = CountVectorizer().fit(corpus)
         bag_of_words = vec.transform(corpus)
@@ -28,7 +32,7 @@ class IMDBContentBased:
                             reverse=True)
         return words_freq[:n]
 
-    def  tf_id(self, column, max_features):
+    def tf_id(self, column, max_features,n_min, n_max):
         df_imdb = self.df_imdb.copy()
         df_imdb["Word_count"] = df_imdb[column].apply(lambda x: len(str(x).split(" ")))
         stop_words = set(stopwords.words("english"))
@@ -57,7 +61,7 @@ class IMDBContentBased:
                                                                 stop_words]
             text = " ".join(text)
             corpus.append(text)
-        cv = CountVectorizer(max_df=0.8, stop_words=stop_words, max_features=max_features, ngram_range=(1, 4))
+        cv = CountVectorizer(max_df=0.8, stop_words=stop_words, max_features=max_features, ngram_range=(n_min, n_max))
         X = cv.fit_transform(corpus)
         tfidf_transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
         tfidf_transformer.fit(X)
@@ -65,20 +69,32 @@ class IMDBContentBased:
         return pd.DataFrame(tf_idf_X.toarray(), index=df_imdb.Title, columns=cv.get_feature_names())
 
     def item_description(self):
-
-        desc_feats = self.tf_id("Description", 1000)
+        df_imdb = self.df_imdb.copy()
+        desc_feats = self.tf_id("Description", 1000,1,  3)
         self.X[desc_feats.columns] = desc_feats
-        directors_feats = pd.get_dummies(self.df_imdb['Director'])*0.4
-        directors_feats.index = self.df_imdb.Title
-        self.X[directors_feats.columns] = directors_feats
-        #title_feats = self.tf_id("Title", 1000)
-        #directors_feats = self.tf_id("Director", 1000)
-        #actors_feats = self.tf_id("Actors", 1000)
-        #self.X[title_feats.columns] = title_feats
-        #self.X[directors_feats.columns] = directors_feats
-        #self.X[actors_feats.columns] = actors_feats
-        print(self.X.isnull().values.any())
+        directors_feats = pd.get_dummies(df_imdb['Director'])*0.4
+        directors_feats.index = df_imdb.Title
+        df_imdb.index = df_imdb.Title
+        self.X["Runtime (Minutes)"] = ((df_imdb["Runtime (Minutes)"].max() - df_imdb["Runtime (Minutes)"]) / (df_imdb["Runtime (Minutes)"].max() - df_imdb["Runtime (Minutes)"].min()))*0.3
+        self.X["Year"] = ((df_imdb["Year"].max() - df_imdb["Year"]) / (df_imdb["Year"].max() - df_imdb["Year"].min()))*0.3
+        self.X["Rating"] = df_imdb["Rating"]*0.1
+        title_feats = self.tf_id("Title", 100, 1, 3)
+        self.X[title_feats.columns] = title_feats
+        actors_feats = self.tf_id("Actors", 1000, 2, 2)
+        self.X[actors_feats.columns] = actors_feats
+        genre_feats = self.tf_id("Genre", 20, 1, 1)
+        self.X[genre_feats.columns] = genre_feats
 
+    def colect_evaluations(self, n_samples=5):
+        sample = self.X.sample(n_samples)
+        for filme in sample.index:
+           nota = np.float(input("Qual a sua nota para o filme " + str(filme) + "?"))
+           if nota != -1:
+                self.y.append(nota)
+                self.X = self.X.drop(index=filme)
+           else:
+               sample = sample.drop(index=filme)
+        self.X_train = self.X_train.append(sample)
     def profile_learning(self):
         return None
 
